d1db919f48d9862ce29366075bfd1c06115cbec7
AbeYuki <aim4highergg@gmail.com>
2023-11-25 12:42:59 +0900

    modify async

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
 from fastapi import FastAPI, Response
 from prometheus_client import CollectorRegistry, Gauge, generate_latest, CONTENT_TYPE_LATEST
-from datetime import datetime
-import psutil,logging
+from os import environ
+import psutil, logging, asyncio
 
 logger = logging.getLogger('uvicorn')
 
 app = FastAPI()
 
-def fetch_metrics():
+# 非同期関数で温度を取得
+async def fetch_temp(registry):
     try:
         temperatures = psutil.sensors_temperatures()
         if not temperatures:
             logger.info("温度情報は利用できません。")
             return None
-        else:
-            for name, entries in temperatures.items():
-                for entry in entries:
-                    # センサー名と温度をメトリクスに追加
-                    CPU_TEMP.labels(sensor=name).set(entry.current)
-                    logger.info(f"{name}: {entry.current}°C")
-    except RuntimeError as e:
-        logger.error(f"エラーが発生しました: {e}")
-        return None
+        for name, entries in temperatures.items():
+            for entry in entries:
+                cpu_temp_gauge = Gauge('cpu_temperature', 'Temperature of the CPU', ['sensor'], registry=registry)
+                cpu_temp_gauge.labels(sensor=name).set(entry.current)
+                logger.info(f"{name}: {entry.current}°C")
+    except Exception as e:
+        logger.error(f"温度取得時にエラーが発生しました: {e}")
 
-# CollectorRegistryのインスタンスを作成
-registry = CollectorRegistry()
-
-# Gaugeのインスタンスを作成し、registryに登録する
-if 'cpu_temperature' not in registry._names_to_collectors:
-    # センサー名でラベルを追加
-    CPU_TEMP = Gauge('cpu_temperature', 'Temperature of the CPU', ['sensor'], registry=registry)
-
+# メトリクスの取得
 @app.get("/metrics")
-def get_metrics():
-    fetch_metrics() 
+async def get_metrics():
+    registry = CollectorRegistry()
+    await fetch_temp(registry)
     return Response(generate_latest(registry), media_type=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    from os import environ
+
+    host = environ.get("HOST_IP", "0.0.0.0")
+    port = int(environ.get("PORT", 8000))
+
+    uvicorn.run("main:app", host=host, port=port, reload=True)
